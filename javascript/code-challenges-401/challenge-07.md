231dab746a01260baf4adec9f9960a1f03b104ef
George Mead <gmeadiv@gmail.com>
2021-10-26 20:44:21 -0700

    adds kthFromEnd function

---
+++ javascript/code-challenges-401/challenge-07.md
@@ -0,0 +1,18 @@
+# Feature Tasks
+
+Write the following method for the Linked List class:
+
+- kth from end
+  - argument: a number, k, as a parameter.
+  - Return the node’s value that is k places from the tail of the linked list.
+  - You have access to the Node class and all the properties on the Linked List class as well as the methods created in previous challenges
+
+## Example
+
+Input | Argument | Output
+
+- head -> [1] -> [3] -> [8] -> [2] -> X | 0 | 2
+
+- head -> [1] -> [3] -> [8] -> [2] -> X | 2 | 3
+
+- head -> [1] -> [3] -> [8] -> [2] -> X | 6 | Exception
---
+++ javascript/code-challenges-401/challenge-07.test.js
@@ -0,0 +1,82 @@
+'use strict';
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
+
+const makeNode = (value, list) => {
+
+  if (!list.head) {
+
+    list.head = new Node(value);
+
+  } else if (list.head) {
+
+    let current = list.head;
+
+    if (!current.next) {
+
+      current.next = new Node(value);
+
+    } else if (current.next) {
+
+      while (current.next !== null) {
+
+        current = current.next;
+
+      }
+
+      current.next = new Node(value);
+
+    }
+  }
+};
+
+const numberList = new LinkedList();
+
+makeNode(1, numberList);
+makeNode(3, numberList);
+makeNode(8, numberList);
+makeNode(2, numberList);
+
+let kthFromEnd = (k, list) => {
+
+  let nodeArray = [];
+  let nodeValue = 'Exception';
+
+  let makeNodeArray = (current) => {
+
+    if (current) {
+
+      if (current.value !== null) {
+        nodeArray.push(current.value);
+      }
+
+      makeNodeArray(current.next);
+
+    }
+  };
+
+  makeNodeArray(list.head);
+
+  let index = nodeArray.length - 1;
+
+  console.log(index, k);
+
+  if (index < k) { console.log('Exception'); }
+  else { nodeValue = nodeArray[index - k]; }
+
+  return nodeValue;
+};
+
+kthFromEnd(0, numberList);
+kthFromEnd(2, numberList);
