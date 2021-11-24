75f0a59b911cc1e1aa47a8b4e331756f86f7729b
George Mead <gmeadiv@gmail.com>
2021-11-24 08:01:20 -0800

    adds attempt

---
+++ javascript/code-challenges-401/merge-sort.test.js
@@ -0,0 +1,57 @@
+'use strict';
+
+let mergeSort = (array) => {
+  let n = array.length;
+
+  if (n > 1) {
+
+    let left = [];
+    let right = [];
+    let mid = n / 2;
+
+    console.log(mid, '<-- mid --<<');
+
+    merge(left, right, array);
+  }
+
+  return array;
+};
+
+let merge = (left, right, array) => {
+
+  let i = 0;
+  let j = 0;
+  let k = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] === right[j]) {
+      array[k] = left[i];
+      i = i + 1;
+    } else {
+      array[k] = right[j];
+    }
+
+    k = k + 1;
+  }
+
+  if (i === left.length) {
+    right.map(number => {
+      array.push(number);
+    });
+  } else {
+    left.map(number => {
+      array.push(number);
+    });
+  }
+};
+
+describe('Testing mergeSort function', () => {
+
+  test('It should sort an array', () => {
+
+    let testArray = [8,4,23,42,16,15];
+
+    expect(mergeSort(testArray)).toStrictEqual([8,4,23,42,16,15]);
+
+  });
+});
