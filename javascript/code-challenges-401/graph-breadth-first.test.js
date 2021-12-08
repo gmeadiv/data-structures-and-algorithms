ed60e6c247fd73f841575f82fefd752fca7b5392
George Mead <gmeadiv@gmail.com>
2021-12-08 15:18:22 -0800

    creates and passes test for searching graph by breadth

---
+++ javascript/code-challenges-401/graph-breadth-first.test.js
@@ -0,0 +1,120 @@
+'use strict';
+
+class Vertex {
+  constructor(value) {
+    this.value = value;
+  }
+}
+
+class Edge {
+  constructor(vertex, weight) {
+    this.vertex = vertex;
+    this.weight = weight;
+  }
+}
+
+class Graph {
+  constructor() {
+    this.adjacencyList = new Map();
+  }
+
+  addVertex(value) {
+    let payload = new Vertex(value);
+    this.adjacencyList.set(payload, []);
+
+    return payload;
+  }
+
+  addDirectedEdge(startVertex, endVertex, weight = 0) {
+
+    if (!this.adjacencyList.has(startVertex) && !this.adjacencyList.has(endVertex)) {
+      throw new Error('vertex error');
+    }
+
+    let neighbors = this.adjacencyList.get(startVertex);
+    let newEdge = new Edge(endVertex, weight);
+    neighbors.push(newEdge);
+
+  }
+
+  getNeighbors(vertex) {
+    if (!this.adjacencyList.has(vertex)) {
+      throw new Error('get neighbor error');
+    }
+
+    return [...this.adjacencyList.get(vertex)];
+  }
+
+  breadthFirst(startVertex) {
+
+    const queue = [];
+    const visitedNodes = new Set();
+
+    queue.push(startVertex);
+    visitedNodes.add(startVertex);
+
+    while (queue.length) {
+      const current = queue.shift();
+
+      let neighbors = this.getNeighbors(current);
+
+      for (let edge of neighbors) {
+
+        let neighbor = edge.vertex;
+
+        if (!visitedNodes.has(neighbor)) {
+          queue.push(neighbor);
+          visitedNodes.add(neighbor);
+        } else { continue }
+      }
+    }
+
+    let visitedNodesArray = []
+
+    visitedNodes.forEach(value => {visitedNodesArray.push(value)})
+    return visitedNodesArray;
+  }
+}
+
+let graph = new Graph();
+
+let A = graph.addVertex('A');
+let B = graph.addVertex('B');
+let C = graph.addVertex('C');
+let D = graph.addVertex('D');
+let E = graph.addVertex('E');
+let F = graph.addVertex('F');
+let G = graph.addVertex('G');
+let H = graph.addVertex('H');
+
+
+graph.addDirectedEdge(A, D);
+graph.addDirectedEdge(A, C);
+graph.addDirectedEdge(D, F);
+graph.addDirectedEdge(B, C);
+graph.addDirectedEdge(B, E);
+graph.addDirectedEdge(C, F);
+graph.addDirectedEdge(C, E);
+graph.addDirectedEdge(C, B);
+graph.addDirectedEdge(F, G);
+graph.addDirectedEdge(G, H);
+graph.addDirectedEdge(H, F);
+
+describe('Testing graph implementation', () => {
+  test('It should be able to create a graph', () => {
+
+    expect(graph.adjacencyList.size).toStrictEqual(8);
+
+  });
+
+  test('It should be able to return all the nodes in the graph', () => {
+
+    let results = graph.breadthFirst(A);
+
+    console.log(JSON.stringify(results), '<-- log');
+
+
+    expect(JSON.stringify(results)).toBe('[{"value":"A"},{"value":"D"},{"value":"C"},{"value":"F"},{"value":"E"},{"value":"B"},{"value":"G"},{"value":"H"}]');
+
+  });
+});
